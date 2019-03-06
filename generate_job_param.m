dc0b8c1381bd7e392e0dc2466c3799b9fa95a486
ifgovh <ifgovh@163.com>
2019-03-06 17:03:40 +1100

    delete the double quation marks

--- generate_job_param.m
+++ generate_job_param.m
@@ -1,5 +1,5 @@
 % generate parameter files for pbs array job
-loss_fun_pool = {'"mse"','"l1"','"nll"','"smooth_l1"','"kl_div"','"cross_entropy"'};
+loss_fun_pool = {'mse','l1','nll','smooth_l1','kl_div','cross_entropy'};
 fileID = fopen('job_params','w');
 for ii = 1:length(loss_fun_pool)
     for jj = 1:length(loss_fun_pool)
