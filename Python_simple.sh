2eede560680ac67ee86dc87885b11199930a97aa
ifgovh <ifgovh@163.com>
2019-03-07 08:45:01 +1100

    swipe batch_size and num_glimpse

--- Python_simple.sh
+++ Python_simple.sh
@@ -2,11 +2,11 @@
 #PBS -P cortical
 #PBS -N simple_python
 #PBS -q defaultQ 
-#PBS -l select=1:ncpus=2:mem=2gb
+#PBS -l select=1:ncpus=2:mem=4gb
 #PBS -l walltime=23:59:59
 #PBS -e PBSout_GPU/
 #PBS -o PBSout_GPU/
-#PBS -J 1-36
+#PBS -J 1-45
 
 #module load python/3.6.5
 cd ~
@@ -14,6 +14,6 @@ source tf/bin/activate
 cd "$PBS_O_WORKDIR"
 params=`sed "${PBS_ARRAY_INDEX}q;d" job_params`
 param_array=( $params )
-python3 main.py --loc_hidden=192 --hidden_size=320 --loss_fun_action=${param_array[0]} --loss_fun_baseline=${param_array[1]} --dataset_name='CIFAR' --patch_size=10 --epochs=2000 --train_patience=1000 --PBSarray_ID=${PBS_ARRAY_INDEX}
+python3 main.py --loc_hidden=192 --hidden_size=320 --batch_size=${param_array[0]} --num_glimpse=${param_array[1]} --dataset_name='CIFAR' --patch_size=10 --epochs=2000 --train_patience=1000 --PBSarray_ID=${PBS_ARRAY_INDEX}
 
 #--batch_szie= --loc_hidden=192 --hidden_size=320 --glimpse_hidden= --num_glimpse= --glimpse_scale= --loss_fun_action= --loss_fun_baseline= 
--- generate_job_param.m
+++ generate_job_param.m
@@ -1,9 +1,15 @@
 % generate parameter files for pbs array job
-loss_fun_pool = {'mse','l1','nll','smooth_l1','kl_div','cross_entropy'};
+% loss_fun_pool = {'mse','l1','nll','smooth_l1','kl_div','cross_entropy'};
 fileID = fopen('job_params','w');
-for ii = 1:length(loss_fun_pool)
-    for jj = 1:length(loss_fun_pool)
-        fprintf(fileID, [loss_fun_pool{ii},' ',loss_fun_pool{jj},'\n']);
+% for ii = 1:length(loss_fun_pool)
+%     for jj = 1:length(loss_fun_pool)
+%         fprintf(fileID, [loss_fun_pool{ii},' ',loss_fun_pool{jj},'\n']);
+%     end
+% end
+
+for ii = 2.^(1:9)%batch_size
+    for jj = 5:5:25%num_glimpse
+        fprintf(fileID, [num2str(ii),' ',num2str(jj),'\n']);
     end
 end
 fclose(fileID);
