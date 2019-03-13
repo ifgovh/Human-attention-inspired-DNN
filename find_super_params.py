3e9154a05f842b95a04414062babc7ee65aef347
ifgovh <ifgovh@163.com>
2019-03-14 10:09:09 +1100

    add __main__
    The code was un-guarded, i.e no if __name__, which causes erro: concurrent.futures.process.BrokenProcessPool: A process in the process pool was terminated abruptly while the future was running or pending.

--- find_super_params.py
+++ find_super_params.py
@@ -4,7 +4,7 @@ import numpy as np
 from nevergrad.optimization import optimizerlib
 from main import main
 
-def find_super_params(patch_size=8, num_patches=1, loc_hidden=256, glimpse_hidden=128, 
+def call_rva(patch_size=8, num_patches=1, loc_hidden=256, glimpse_hidden=128, 
 	num_glimpses=6, std=0.17, M=10, valid_size=0.1, batch_size=256, batchnorm_flag_phi=True,
 	batchnorm_flag_l=True, batchnorm_flag_g=True, batchnorm_flag_h=True, glimpse_scale=2, weight_decay=0,
 	dropout_phi=0, dropout_l=0,  dropout_g=0, dropout_h=0):
@@ -147,94 +147,97 @@ def find_super_params(patch_size=8, num_patches=1, loc_hidden=256, glimpse_hidde
 
 	return 100 - best_model_file['best_valid_acc']
 
+def find_super_params():
+	# Instrumentation
+	# argument transformation
+	"""
+	def find_super_params(patch_size=8, num_patches=1, loc_hidden=256, glimpse_hidden=128, 
+		num_glimpses=6, std=0.17, M=10, valid_size=0.1, batch_size=256, batchnorm_phi=True,
+		batchnorm_l=True, batchnorm_g=True, batchnorm_h=True, glimpse_scale=2, weight_decay=0,
+		dropout_phi=0, dropout_l=0,  dropout_g=0, dropout_h=0):
 
-# Instrumentation
-# argument transformation
-"""
-def find_super_params(patch_size=8, num_patches=1, loc_hidden=256, glimpse_hidden=128, 
-	num_glimpses=6, std=0.17, M=10, valid_size=0.1, batch_size=256, batchnorm_phi=True,
-	batchnorm_l=True, batchnorm_g=True, batchnorm_h=True, glimpse_scale=2, weight_decay=0,
-	dropout_phi=0, dropout_l=0,  dropout_g=0, dropout_h=0):
+	When optimizing hyperparameters as e.g. in machine learning. If you don't know what variables (see instrumentation) to use:
 
-When optimizing hyperparameters as e.g. in machine learning. If you don't know what variables (see instrumentation) to use:
+	use SoftmaxCategorical for discrete variables
+	use TwoPointsDE with num_workers equal to the number of workers available to you. See the machine learning example for more.
+	Or if you want something more aimed at robustly outperforming random search in highly parallel settings (one-shot):
 
-use SoftmaxCategorical for discrete variables
-use TwoPointsDE with num_workers equal to the number of workers available to you. See the machine learning example for more.
-Or if you want something more aimed at robustly outperforming random search in highly parallel settings (one-shot):
+	use OrderedDiscrete for discrete variables, taking care that the default value is in the middle.
+	Use ScrHammersleySearchPlusMiddlePoint (PlusMiddlePoint only if you have continuous parameters or good default values for discrete parameters).
+	"""
+	# dicrete
+	patch_size = inst.var.SoftmaxCategorical(np.arange(5,20).tolist()) 
+	num_patches = inst.var.SoftmaxCategorical(np.arange(1,5).tolist()) 
+	num_glimpses = inst.var.SoftmaxCategorical(np.arange(5,15).tolist())
+	# glimpse_hidden = inst.var.SoftmaxCategorical(np.arange(128,5)) 
+	# loc_hidden = inst.var.SoftmaxCategorical(np.arange(192,15))
 
-use OrderedDiscrete for discrete variables, taking care that the default value is in the middle.
-Use ScrHammersleySearchPlusMiddlePoint (PlusMiddlePoint only if you have continuous parameters or good default values for discrete parameters).
-"""
-# dicrete
-patch_size = inst.var.SoftmaxCategorical(np.arange(5,20).tolist()) 
-num_patches = inst.var.SoftmaxCategorical(np.arange(1,5).tolist()) 
-num_glimpses = inst.var.SoftmaxCategorical(np.arange(5,15).tolist())
-# glimpse_hidden = inst.var.SoftmaxCategorical(np.arange(128,5)) 
-# loc_hidden = inst.var.SoftmaxCategorical(np.arange(192,15))
+	batchnorm_phi = inst.var.SoftmaxCategorical(["True", "False"])
+	batchnorm_l = inst.var.SoftmaxCategorical(["True", "False"])
+	batchnorm_g = inst.var.SoftmaxCategorical(["True", "False"])
+	batchnorm_h = inst.var.SoftmaxCategorical(["True", "False"])
 
-batchnorm_phi = inst.var.SoftmaxCategorical(["True", "False"])
-batchnorm_l = inst.var.SoftmaxCategorical(["True", "False"])
-batchnorm_g = inst.var.SoftmaxCategorical(["True", "False"])
-batchnorm_h = inst.var.SoftmaxCategorical(["True", "False"])
+	glimpse_scale = inst.var.SoftmaxCategorical(np.arange(1,3,1).tolist())
+	weight_decay = inst.var.SoftmaxCategorical(np.arange(0.0001,0.05,0.0005).tolist())
+	dropout_phi = inst.var.SoftmaxCategorical(np.arange(0.1,0.9,0.1).tolist())
+	dropout_l = inst.var.SoftmaxCategorical(np.arange(0.1,0.9,0.1).tolist())
+	dropout_g = inst.var.SoftmaxCategorical(np.arange(0.1,0.9,0.1).tolist())
+	dropout_h = inst.var.SoftmaxCategorical(np.arange(0.1,0.9,0.1).tolist())
+	 
 
-glimpse_scale = inst.var.SoftmaxCategorical(np.arange(1,3,1).tolist())
-weight_decay = inst.var.SoftmaxCategorical(np.arange(0.0001,0.05,0.0005).tolist())
-dropout_phi = inst.var.SoftmaxCategorical(np.arange(0.1,0.9,0.1).tolist())
-dropout_l = inst.var.SoftmaxCategorical(np.arange(0.1,0.9,0.1).tolist())
-dropout_g = inst.var.SoftmaxCategorical(np.arange(0.1,0.9,0.1).tolist())
-dropout_h = inst.var.SoftmaxCategorical(np.arange(0.1,0.9,0.1).tolist())
- 
+	# continuous; the Gaussian method does not have truncated version, so it is unavailable
+	# glimpse_scale = inst.var.Gaussian(mean=2, std=2)  
+	# weight_decay = inst.var.Gaussian(mean=0.001, std=0.001)
+	# dropout_phi = inst.var.Gaussian(mean=0.5, std=2)
+	# dropout_l = inst.var.Gaussian(mean=0.5, std=2)
+	# dropout_g = inst.var.Gaussian(mean=0.5, std=2)
+	# dropout_h = inst.var.Gaussian(mean=0.5, std=2)
+	"""
+	def find_super_params(patch_size=8, num_patches=1, loc_hidden=256, glimpse_hidden=128, 
+		num_glimpses=6, std=0.17, M=10, valid_size=0.1, batch_size=256, batchnorm_phi=True,
+		batchnorm_l=True, batchnorm_g=True, batchnorm_h=True, glimpse_scale=2, weight_decay=0,
+		dropout_phi=0, dropout_l=0,  dropout_g=0, dropout_h=0):
+	"""
+	# create the instrumented function
+	# put them in order, if it is discrete varible, only give the variable name; if it is continuous, give a pair;
+	# if it is constant, only give the constant
+	instrum = inst.Instrumentation(patch_size,  num_patches, 256, 128, num_glimpses,
+		0.17, 10, 0.1, 256, batchnorm_phi, batchnorm_l, batchnorm_g, batchnorm_h, 
+		glimpse_scale, weight_decay, dropout_phi,  
+		dropout_l, dropout_g, dropout_h)
 
-# continuous; the Gaussian method does not have truncated version, so it is unavailable
-# glimpse_scale = inst.var.Gaussian(mean=2, std=2)  
-# weight_decay = inst.var.Gaussian(mean=0.001, std=0.001)
-# dropout_phi = inst.var.Gaussian(mean=0.5, std=2)
-# dropout_l = inst.var.Gaussian(mean=0.5, std=2)
-# dropout_g = inst.var.Gaussian(mean=0.5, std=2)
-# dropout_h = inst.var.Gaussian(mean=0.5, std=2)
-"""
-def find_super_params(patch_size=8, num_patches=1, loc_hidden=256, glimpse_hidden=128, 
-	num_glimpses=6, std=0.17, M=10, valid_size=0.1, batch_size=256, batchnorm_phi=True,
-	batchnorm_l=True, batchnorm_g=True, batchnorm_h=True, glimpse_scale=2, weight_decay=0,
-	dropout_phi=0, dropout_l=0,  dropout_g=0, dropout_h=0):
-"""
-# create the instrumented function
-# put them in order, if it is discrete varible, only give the variable name; if it is continuous, give a pair;
-# if it is constant, only give the constant
-instrum = inst.Instrumentation(patch_size,  num_patches, 256, 128, num_glimpses,
-	0.17, 10, 0.1, 256, batchnorm_phi, batchnorm_l, batchnorm_g, batchnorm_h, 
-	glimpse_scale, weight_decay, dropout_phi,  
-	dropout_l, dropout_g, dropout_h)
+	print(instrum.dimension)  
 
-print(instrum.dimension)  
-
-#Converts data to arguments to check
-#print(instrum.data_to_arguments([1, -80, -80, 80, 3]))
-# prints (args, kwargs): (('b', 'e', 'blublu'), {'value': 7})
-# b is selected because 1 > 0 (the threshold is 0 here since there are 2 values.
-# e is selected because proba(e) = exp(80) / (exp(80) + exp(-80) + exp(-80))
-# value=7 because 3 * std + mean = 7
+	#Converts data to arguments to check
+	#print(instrum.data_to_arguments([1, -80, -80, 80, 3]))
+	# prints (args, kwargs): (('b', 'e', 'blublu'), {'value': 7})
+	# b is selected because 1 > 0 (the threshold is 0 here since there are 2 values.
+	# e is selected because proba(e) = exp(80) / (exp(80) + exp(-80) + exp(-80))
+	# value=7 because 3 * std + mean = 7
 
 
-# create the instrumented function using the "Instrumentation" instance above
-ifunc = instrum.instrument(find_super_params)
-print(ifunc.dimension)  # dimensional space as above
-# you can still access the instrumentation instance will ifunc.instrumentation
+	# create the instrumented function using the "Instrumentation" instance above
+	ifunc = instrum.instrument(call_rva)
+	print(ifunc.dimension)  # dimensional space as above
+	# you can still access the instrumentation instance will ifunc.instrumentation
 
-optimizer = optimizerlib.PortfolioDiscreteOnePlusOne(dimension=ifunc.dimension, 
-	budget=50, num_workers=4) #TwoPointsDE
-# simple example
-#recommendation = optimizer.optimize(ifunc)
-# using several workers
-from concurrent import futures
-with futures.ProcessPoolExecutor(max_workers=optimizer.num_workers) as executor:
-    recommendation = optimizer.optimize(ifunc, executor=executor, batch_mode=False)
+	optimizer = optimizerlib.PortfolioDiscreteOnePlusOne(dimension=ifunc.dimension, 
+		budget=50, num_workers=4) #TwoPointsDE
+	# simple example
+	#recommendation = optimizer.optimize(ifunc)
+	# using several workers
+	from concurrent import futures
+	with futures.ProcessPoolExecutor(max_workers=optimizer.num_workers) as executor:
+	    recommendation = optimizer.optimize(ifunc, executor=executor, batch_mode=False)
 
-# # recover the arguments this way (don't forget deteriministic=True)
-# args, kwargs = ifunc.data_to_arguments(recommendation, deterministic=True)
-# print(args)    # should print ["b", "e", "blublu"]
-# print(kwargs)  # should print {"value": 0} because -.5 * std + mean = 0
+	# # recover the arguments this way (don't forget deteriministic=True)
+	# args, kwargs = ifunc.data_to_arguments(recommendation, deterministic=True)
+	# print(args)    # should print ["b", "e", "blublu"]
+	# print(kwargs)  # should print {"value": 0} because -.5 * std + mean = 0
 
-# but be careful, since some variables are stochastic (SoftmaxCategorical ones are), setting deterministic=False may yield different results
-# The following will print more information on the conversion to your arguments:
-print(ifunc.get_summary(recommendation))
+	# but be careful, since some variables are stochastic (SoftmaxCategorical ones are), setting deterministic=False may yield different results
+	# The following will print more information on the conversion to your arguments:
+	print(ifunc.get_summary(recommendation))
+
+if __name__ == '__main__':
+	find_super_params()
