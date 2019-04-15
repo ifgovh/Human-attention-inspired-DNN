73d960b08362e0d0da751facce763da99208e684
ifgovh <ifgovh@163.com>
2019-04-16 09:50:30 +1000

    update

--- CPU_training.sh
+++ CPU_training.sh
@@ -14,8 +14,6 @@ source tf/bin/activate
 cd "$PBS_O_WORKDIR"
 params=`sed "${PBS_ARRAY_INDEX}q;d" job_params`
 param_array=( $params )
-python3 main.py --loc_hidden=256 --hidden_size=384 --batch_size=256 --num_glimpse=${param_array[0]}\
- --weight_decay=${param_array[1]}  --dataset_name='CIFAR' --patch_size=10 --epochs=2000 --train_patience=1000\
-  --PBSarray_ID=${PBS_ARRAY_INDEX} --use_gpu=False
+python3 main.py --loc_hidden=256 --hidden_size=384 --batch_size=256 --num_glimpse=${param_array[0]} --weight_decay=${param_array[1]}  --dataset_name='CIFAR' --patch_size=10 --epochs=2000 --train_patience=1000 --PBSarray_ID=${PBS_ARRAY_INDEX} --use_gpu=False
 
 #--batch_szie= --loc_hidden=192 --hidden_size=320 --glimpse_hidden= --num_glimpse= --glimpse_scale= --loss_fun_action= --loss_fun_baseline= 
