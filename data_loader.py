2fff865091462f65cc00573fb76b6fd12013beac
Guozhang Chen <ifgovh@163.com>
2019-06-30 22:53:20 +1000

    update cluttered method

--- data_loader.py
+++ data_loader.py
@@ -6,6 +6,78 @@ import torch
 from torchvision import datasets
 from torchvision import transforms
 from torch.utils.data.sampler import SubsetRandomSampler
+from torch.utils.data.dataset import Dataset
+
+class MyCustomDataset(Dataset):
+    # clutered MNIST
+    def __init__(self, ...):                
+        # define transforms
+        normalize = transforms.Normalize((0.1307,), (0.3081,))
+        self.trans = transforms.Compose([transforms.ToTensor(), normalize])
+        
+    def __getitem__(self, index):
+        # get raw data from pytorch built-in fun
+        dataset = datasets.MNIST(
+            data_dir, train=True, download=True, transform=trans
+        )
+
+        raw_data = torch.utils.data.DataLoader(
+            dataset = dataset, batch_size=9, shuffle=shuffle,
+            num_workers=num_workers, pin_memory=pin_memory
+        )
+        
+        for i, (x, y) in enumerate(self.train_loader):
+            img[i] = clutter(x)
+
+
+        # stuff
+        ...
+        data = # Some data read from a file or image
+        
+        # When you call the transform for the second time it calls __call__() and applies the transform 
+        data = self.center_crop(data)  # (2)
+        data = self.to_tensor(data)  # (2)
+        
+        # Or you can call the composed version
+        data = self.trasnformations(data)  # (3)
+        
+        # Note that you only need one of the implementations, (2) or (3)
+        return (img, label)
+
+    def __len__(self):
+        return count # of how many data(images?) you have
+
+    def clutter(batch, train_data, width=60, height=60, n_patches=4):
+    """Inserts MNIST digits at random locations in larger blank background and
+    adds 8 by 8 subpatches from other random MNIST digits."""
+
+    # get dimensions
+    n, width_img, height_img, c_img = batch.shape
+    width_sub, height_sub           = 8,8 # subpatch
+
+    assert n > 4, 'There must be more than 4 images in the batch (there are {})'.format(n)
+
+    data    = np.zeros((n, width, height, c_img)) # blank background for each image
+
+    for k in range(n):
+
+        # sample location
+        x_pos   = np.random.randint(0,width - width_img)
+        y_pos   = np.random.randint(0,height - height_img)
+
+        # insert in blank image
+        data[k, x_pos:x_pos+width_img, y_pos:y_pos+height_img, :] += batch[k]
+
+        # add 8 x 8 subpatches from random other digits
+        for i in range(n_patches):
+            digit   = train_data[np.random.randint(0, train_data.shape[0]-1)]
+            c1, c2  = np.random.randint(0, width_img - width_sub, size=2)
+            i1, i2  = np.random.randint(0, width - width_sub, size=2)
+            data[k, i1:i1+width_sub, i2:i2+height_sub, :] += digit[c1:c1+width_sub, c2:c2+height_sub, :]
+
+    data = np.clip(data, 0., 1.)
+
+    return data
 
 
 def get_train_valid_loader(data_dir,
